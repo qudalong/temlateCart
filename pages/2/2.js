81f257b3625f5fde7c1c297d84f87803663dbe4b
qudalong <862792055@qq.com>
2018-11-26 22:51:13 +0800

    xx

--- pages/2/2.js
+++ pages/2/2.js
@@ -10,42 +10,79 @@ Page({
    */
   data: {
     list: [],
-    cartList: []
+    goods: [],
+    selectFoods: []
   },
   onLoad: function(options) {
+    var that=this
     wx.request({
       url: 'https://mp.ucloudant.com/app/index.php?i=56&t=0&v=9.2&from=wxapp&c=entry&a=wxapp&do=Dishes&m=zh_dianc&sign=819fcd817f0aeb118075924d12978351&id=5&dishes_type=2',
       method: 'GET',
       success: (res) => {
-        this.setData({
-          list: res.data
+        that.setData({
+          list: res.data,
         })
-        // console.log(this.data.list)
+        for (let i in that.data.list) {
+          for (let j in that.data.list[i].goods) {
+            let count = `list[${i}].goods[${j}].count`
+            that.setData({
+              [count]:0
+            })
+          }
+        }
+        // console.log(that.data.list)
       },
     })
   },
+  onSelectFoods() {
+    let foods = []
+    for (let i in this.data.list){
+      for (let j in this.data.list[i].goods) {
+        if (this.data.list[i].goods[j].count){
+          foods.push(this.data.list[i].goods)
+        }
+      }
+    }
+    this.setData({
+      selectFoods: foods
+    })
+    console.log(this.data.selectFoods)
+  },
+
+
+
+
+
   add(e) {
     const food = e.currentTarget.dataset.food
     const parentIndex = e.currentTarget.dataset.parentIndex
     const index = e.currentTarget.dataset.index
     const currentFood = this.data.list[parentIndex].goods[index]
-    if (currentFood.count) {
-      currentFood.count += 1
-      const i = this.data.cartList.findIndex((value)=>{
-        return value.name == currentFood.name
-      })
-      this.data.cartList[i].count+=1
-      console.log('tou')
+    if (food.count) {
+      food.count = food.count + 1
+      console.log(1)
     } else {
-      currentFood.count = 1
-      this.data.cartList.push(currentFood)
-      console.log('mei')
+      food.count = 1
+      console.log(0)
     }
+    // if (currentFood.count) {
+    //   currentFood.count += 1
+    //   const i = this.data.cartList.findIndex((value)=>{
+    //     return value.name == currentFood.name
+    //   })
+    //   this.data.cartList[i].count+=1
+    //   console.log('tou')
+    // } else {
+    //   currentFood.count = 1
+    //   this.data.cartList.push(currentFood)
+    //   console.log('mei')
+    // }
 
 
 
     this.setData({
       list: this.data.list,
+      food: food,
       cartList: this.data.cartList
     })
   },
@@ -62,7 +99,7 @@ Page({
       })
       this.data.cartList[i].count = this.data.cartList[i].count - 1
       if (this.data.cartList[i].count == 0) {
-        this.data.cartList.splice(i,1)
+        this.data.cartList.splice(i, 1)
       }
     }
     this.setData({
@@ -77,9 +114,7 @@ Page({
 
 
   onReady: function() {},
-  onShow: function() {
-    console.log('onshow')
-  },
+  onShow: function() {},
   onHide: function() {
     console.log('onhide')
     wx.showModal({
