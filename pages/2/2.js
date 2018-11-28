036f07bd86f9e3bf02b7570fdfd882915a6079a9
qudalong <862792055@qq.com>
2018-11-28 21:10:01 +0800

    小数计算精度总价格

--- pages/2/2.js
+++ pages/2/2.js
@@ -1,4 +1,6 @@
-// pages/2/2.js
+const utils = require('../../utils/util');
+const multi = utils.floatOpration.multi;
+const add = utils.floatOpration.add;
 Page({
   go() {
     wx.navigateTo({
@@ -14,6 +16,7 @@ Page({
     navActive: 0,
     heightArr: [],
     containerH: 0,
+    totalMoeny:0,
     closeShadow: false
   },
   onLoad: function(options) {
@@ -67,6 +70,7 @@ Page({
     }
   },
 
+  //清空购物车
   clearShopCart() {
     let data = this.data.list;
     let goods = this.data.selectFoods;
@@ -79,7 +83,8 @@ Page({
 
     this.setData({
       selectFoods: [],
-      list: this.data.list
+      list: this.data.list,
+      totalMoney:0
     })
   },
 
@@ -145,6 +150,21 @@ Page({
       list,
       selectFoods
     })
+    this.calculateMoney(selectFoods)
+  },
+
+  /*计算价格*/
+  calculateMoney(selectFoods) {
+    let totalMoney = 0;
+    let price = 0;
+    for (let i = 0; i < selectFoods.length; i++) {
+      price = Number(selectFoods[i].money);
+      totalMoney = add(totalMoney, multi(price, selectFoods[i].count));
+      // totalMoney += selectFoods[i].count * price;
+    }
+    this.setData({
+      totalMoney: totalMoney
+    });
   },
 
   onReady: function() {
